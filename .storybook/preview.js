9331d60d2d20bac4b526938ff6a5f4ea455b5d22
Raveena Ingale <ingale.r@northeastern.edu>
2024-11-18 10:30:05 -0500

    css-learning

---
+++ .storybook/preview.js
@@ -0,0 +1,16 @@
+/** @type { import('@storybook/html').Preview } */
+
+import '../css/index.css';
+
+const preview = {
+  parameters: {
+    controls: {
+      matchers: {
+        color: /(background|color)$/i,
+        date: /Date$/i,
+      },
+    },
+  },
+};
+
+export default preview;
